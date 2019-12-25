5eeb218d09ff9bcade10624e0b25c19dec77a809
Robomikel <59216792+Robomikel@users.noreply.github.com>
2019-12-25 14:45:02 -0700

    Add files via upload

--- functions/fn_UpdateSteamer.ps1
+++ functions/fn_UpdateSteamer.ps1
@@ -2,5 +2,5 @@
 Function Get-UpdateSteamer {
     (New-Object Net.WebClient).DownloadFile("https://github.com/Robomikel/steamer/archive/master.zip", "steamer.zip")
     Expand-Archive ".\steamer.zip" ".\steamer" -Force
-    Get-ChildItem -Path ".\steamer\steamer-master" -Recurse | Copy-Item -Destination ".\" -Force
+    Copy-Item -Path ".\steamer\steamer-master\*" -Destination ".\" -Recurse -Force
 }
