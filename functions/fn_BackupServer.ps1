24fa6687b381d257fe8a032bf50ff8996ae696e5
Robomikel <59216792+Robomikel@users.noreply.github.com>
2019-12-25 18:43:47 -0700

    Add files via upload

--- functions/fn_BackupServer.ps1
+++ functions/fn_BackupServer.ps1
@@ -1,3 +1,37 @@
+
+Function Get-ChecktaskDisable {
+    Get-ScheduledTask -TaskName "$global:server monitor"  >$null 2>&1
+ # source:
+# http://blogs.msdn.com/b/powershell/archive/2006/09/15/errorlevel-equivalent.aspx
+if($?) {
+    #write-host "True, last operation succeeded"
+    Disable-ScheduledTask -TaskName "$global:server monitor"
+}
+
+if (!$?) {
+    #write-host "Not True, last operation failed"
+    write-host "Scheduled Task does not exist"
+    
+}
+    
+}
+
+Function Get-ChecktaskEnable {
+    Get-ScheduledTask -TaskName "$global:server monitor"  >$null 2>&1
+ # source:
+# http://blogs.msdn.com/b/powershell/archive/2006/09/15/errorlevel-equivalent.aspx
+if($?) {
+    #write-host "True, last operation succeeded"
+    Enable-ScheduledTask -TaskName "$global:server monitor"
+}
+
+if (!$?) {
+    #write-host "Not True, last operation failed"
+    write-host "Scheduled Task does not exist"
+    
+}
+    
+}
 Function New-BackupFolder 
     {
         $path = "$global:currentdir\backups" 
@@ -16,15 +50,17 @@ Function New-BackupServer {
     $BackupDate = get-date -Format yyyyMMdd
     Write-Host '*** Stopping Server Process *****' -ForegroundColor Yellow -BackgroundColor Black  
     & "$global:currentdir\$global:server\Stops-*.ps1"
-    Write-Host '*** disabling scheduled task *****' -ForegroundColor Yellow -BackgroundColor Black 
-    Disable-ScheduledTask -TaskName "$global:server monitor"
+    Write-Host '*** disabling scheduled task *****' -ForegroundColor Yellow -BackgroundColor Black
+    Get-ChecktaskDisable 
+    #Disable-ScheduledTask -TaskName "$global:server monitor"
     New-BackupFolder
     Write-Host '*** Server Backup Started *****' -ForegroundColor Yellow -BackgroundColor Black
     Set-Location $global:currentdir\7za920\ 
     ./7za a $global:currentdir\backups\Backup_$global:server-$BackupDate.zip $global:currentdir\$global:server\*
     #Compress-Archive -Path $global:currentdir\$global:server\* -DestinationPath ("$global:currentdir\$global:server\Backup-$global:server" + (get-date -Format yyyyMMdd) + '.zip') -CompressionLevel Optimal
     #Compress-Archive -Path $global:currentdir\$global:server\* -DestinationPath $global:currentdir\$global:server\Backup-$global:server.zip
-    Enable-ScheduledTask -TaskName "$global:server monitor"
+    Get-ChecktaskEnable
+    #Enable-ScheduledTask -TaskName "$global:server monitor"
     Set-Location $global:currentdir
     #Set-Steamer
 }
