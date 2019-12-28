ecf11faecdb22a6025fab065d82fe420cefeab44
Robomikel <59216792+Robomikel@users.noreply.github.com>
2019-12-28 11:45:13 -0700

    Delete fn_UpdateServer.ps1

--- functions/fn_UpdateServer.ps1
+++
@@ -1,6 +0,0 @@
-Function Select-UpdateServer {
-    Write-Host '*** Checking for Update *****' -ForegroundColor Yellow -BackgroundColor Black  
-    & "$global:currentdir\$global:server\Update-*.ps1"
-    Set-Location $global:currentdir
-    #Select-Steamer
-}
