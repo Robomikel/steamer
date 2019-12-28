79ea4dfa9e630d15497d852a0e9630a4f0022d02
Robomikel <59216792+Robomikel@users.noreply.github.com>
2019-12-28 11:45:24 -0700

    Delete fn_ValidateServer.ps1

--- functions/fn_ValidateServer.ps1
+++
@@ -1,6 +0,0 @@
-Function Select-ValidateServer {
-    Write-Host '*** Validating Server *****' -ForegroundColor Yellow -BackgroundColor Black  
-    & "$global:currentdir\$global:server\Validate-*.ps1"
-    Set-Location $global:currentdir
-    #Select-Steamer
-}
