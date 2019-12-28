f9ff62e41d01f78e2fbc4487347f02d5ce4e14eb
Robomikel <59216792+Robomikel@users.noreply.github.com>
2019-12-28 11:43:39 -0700

    Delete fn_CreateStopsScript.ps1

--- functions/fn_CreateStopsScript.ps1
+++
@@ -1,7 +0,0 @@
-Function New-StopsScript {
-    Write-Host '*** Creating Stop Script *****' -ForegroundColor Yellow -BackgroundColor Black 
-    New-Item $global:currentdir\$global:server\Stops-$global:server.ps1 -Force
-    #Add-Content -Path $global:currentdir\$global:server\Stops-$global:server.ps1 -Value "stop-process -Name '$global:process' -Force"
-    Add-Content -Path $global:currentdir\$global:server\Stops-$global:server.ps1 -Value "if(`$Null -eq (get-process `"$global:process`" -ea SilentlyContinue)){"
-    Add-Content -Path $global:currentdir\$global:server\Stops-$global:server.ps1 -Value "Write-Host `"Not Running`" -ForegroundColor Red -BackgroundColor Black}else{stop-process -Name '$global:process' -Force}"
-}
