5cd1636cee24ecf5f40bf6e7604cbc83d56b0b8a
Robomikel <59216792+Robomikel@users.noreply.github.com>
2019-12-27 17:51:05 -0700

    Add files via upload

--- functions/fn_UpdateSteamer.ps1
+++ functions/fn_UpdateSteamer.ps1
@@ -1,7 +1,7 @@
 
 Function Get-UpdateSteamer {
     Write-Host '*** Downloading Steamer github files *****' -ForegroundColor Yellow -BackgroundColor Black 
-    (New-Object Net.WebClient).DownloadFile("https://github.com/Robomikel/steamer/archive/master.zip", "steamer.zip")
-    Expand-Archive ".\steamer.zip" ".\steamer" -Force
-    Copy-Item -Path ".\steamer\steamer-master\*" -Destination ".\" -Recurse -Force
+    (New-Object Net.WebClient).DownloadFile("https://github.com/Robomikel/steamer/archive/master.zip", "$global:currentdir\steamer.zip")
+    Expand-Archive "$global:currentdir\steamer.zip" "$global:currentdir\steamer" -Force
+    Copy-Item -Path "$global:currentdir\steamer\steamer-master\*" -Destination "$global:currentdir\" -Recurse -Force
 }
