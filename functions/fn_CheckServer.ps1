ce3b2e91c84d66a2af6566ac8a9ef1ed003db9f2
Robomikel <59216792+Robomikel@users.noreply.github.com>
2019-12-28 11:42:31 -0700

    Delete fn_CheckServer.ps1

--- functions/fn_CheckServer.ps1
+++
@@ -1,6 +0,0 @@
-Function Select-CheckServer {
-    Write-Host '*** Checking Process for server *****' -ForegroundColor Yellow -BackgroundColor Black  
-    & "$global:currentdir\$global:server\Check-*.ps1" 
-    Set-Location $global:currentdir
-    #Select-Steamer
-}
