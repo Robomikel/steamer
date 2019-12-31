8f945099caeb081ab1bdefcb9c445b52ba03f209
Robomikel <michaelawilliams28@outlook.com>
2019-12-31 12:13:37 -0700

    Updated Steamer function

--- functions/fn_UpdateSteamer.ps1
+++ functions/fn_UpdateSteamer.ps1
@@ -1,8 +1,14 @@
 
 Function Get-UpdateSteamer {
 
-    Write-Host '*** Downloading Steamer github files *****' -ForegroundColor Yellow -BackgroundColor Black 
-    (New-Object Net.WebClient).DownloadFile("$global:steamerurl", "$global:currentdir\steamer.zip")
+    Write-Host '*** Downloading Steamer github files *****' -ForegroundColor Magenta -BackgroundColor Black 
+    #(New-Object Net.WebClient).DownloadFile("$global:steamerurl", "$global:currentdir\steamer.zip")
+    [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12;
+    Invoke-WebRequest -Uri $global:steamerurl -OutFile $global:currentdir\steamer.zip 
     Expand-Archive "$global:currentdir\steamer.zip" "$global:currentdir\steamer" -Force
     Copy-Item -Path "$global:currentdir\steamer\steamer-master\*" -Destination "$global:currentdir\" -Recurse -Force
+    Write-Host '*** Steamer github files Updated *****' -ForegroundColor Yellow -BackgroundColor Black
+    Write-Host '*** Press Enter to Close this session *****' -ForegroundColor Yellow -BackgroundColor Black
+    Pause  
+    stop-process -Id $PID
 }
