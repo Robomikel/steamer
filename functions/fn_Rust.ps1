5e8e45afd5faa266ef240a852365e466be58bbd3
Robomikel <michaelawilliams28@outlook.com>
2020-01-01 13:13:01 -0700

    Updated Rust launch

--- functions/fn_Rust.ps1
+++ functions/fn_Rust.ps1
@@ -2,24 +2,28 @@ Function New-LaunchScriptRustPS
     {
         #----------   Rust server CDF  -------------------
         $global:process = "RustDedicated"
-        ${global:IP} = Read-host -Prompt 'Input Server local IP'
-        if(($global:PORT = Read-Host "Input Server Port,Press enter to accept default value [28015]") -eq ''){$global:PORT="28015"}else{$global:PORT}
-        if(($global:RCONPORT = Read-Host "Input Server Rcon Port,Press enter to accept default value [28016]") -eq ''){$global:RCONPORT="28016"}else{$global:RCONPORT}
-        if(($global:RCONPASSWORD = Read-Host "Input Server Rcon Password,Press enter to accept default value [CHANGEME]") -eq ''){$global:RCONPASSWORD="CHANGEME"}else{$global:RCONPASSWORD}
-        if(($global:RCONWEB = Read-Host "Input Server Rcon Web,Press enter to accept default value [1]") -eq ''){$global:RCONWEB="1"}else{$global:RCONWEB}
-        $global:HOSTNAME = Read-host -Prompt 'Input Server name'
-        $global:MAXPLAYERS = Read-host -Prompt 'Input maxplayers'
-        if(($global:SEED = Read-Host "Input Server seed,Press enter to accept default value [4125143]") -eq ''){$global:SEED="4125143"}else{$global:SEED}
-        if(($global:WORLDSIZE = Read-Host "Input Server WorldSize,Press enter to accept default value [3000]") -eq ''){$global:WORLDSIZE="3000"}else{$global:WORLDSIZE}
-        if(($global:SAVEINTERVAL = Read-Host "Input Server Save Interval,Press enter to accept default value [300]") -eq ''){$global:SAVEINTERVAL="300"}else{$global:SAVEINTERVAL}
-        if(($global:TICKRATE = Read-Host "Input Server Tickrate,Press enter to accept default value [30]") -eq ''){$global:TICKRATE="30"}else{$global:TICKRATE}
+        Write-Host '*** Configure Instance *****' -ForegroundColor Yellow -BackgroundColor Black
+        Write-Host 'Input Server local IP: ' -ForegroundColor Cyan -NoNewline
+        ${global:IP} = Read-host
+        if(($global:PORT = Read-Host -Prompt (Write-Host "Input Server Port,Press enter to accept default value [28015]: " -ForegroundColor Cyan -NoNewline)) -eq ''){$global:PORT="28015"}else{$global:PORT}
+        if(($global:RCONPORT = Read-Host -Prompt (Write-Host "Input Server Rcon Port,Press enter to accept default value [28016]: " -ForegroundColor Cyan -NoNewline)) -eq ''){$global:RCONPORT="28016"}else{$global:RCONPORT}
+        if(($global:RCONPASSWORD = Read-Host -Prompt (Write-Host "Input Server Rcon Password,Press enter to accept default value [CHANGEME]: " -ForegroundColor Cyan -NoNewline)) -eq ''){$global:RCONPASSWORD="CHANGEME"}else{$global:RCONPASSWORD}
+        if(($global:RCONWEB = Read-Host -Prompt (Write-Host "Input Server Rcon Web,Press enter to accept default value [1]: " -ForegroundColor Cyan -NoNewline)) -eq ''){$global:RCONWEB="1"}else{$global:RCONWEB}
+        Write-Host 'Input Server name: ' -ForegroundColor Cyan -NoNewline
+        $global:HOSTNAME = Read-host
+        Write-Host 'Input maxplayers: ' -ForegroundColor Cyan -NoNewline
+        $global:MAXPLAYERS = Read-host
+        if(($global:SEED = Read-Host -Prompt (Write-Host "Input Server seed,Press enter to accept default value [4125143]: " -ForegroundColor Cyan -NoNewline)) -eq ''){$global:SEED="4125143"}else{$global:SEED}
+        if(($global:WORLDSIZE = Read-Host -Prompt (Write-Host "Input Server WorldSize,Press enter to accept default value [3000]: " -ForegroundColor Cyan -NoNewline)) -eq ''){$global:WORLDSIZE="3000"}else{$global:WORLDSIZE}
+        if(($global:SAVEINTERVAL = Read-Host -Prompt (Write-Host "Input Server Save Interval,Press enter to accept default value [300]: " -ForegroundColor Cyan -NoNewline)) -eq ''){$global:SAVEINTERVAL="300"}else{$global:SAVEINTERVAL}
+        if(($global:TICKRATE = Read-Host -Prompt (Write-Host "Input Server Tickrate,Press enter to accept default value [30]: " -ForegroundColor Cyan -NoNewline)) -eq ''){$global:TICKRATE="30"}else{$global:TICKRATE}
         Write-Host "***  Creating Launch script  ***" -ForegroundColor Cyan -BackgroundColor Black
         New-Item $global:currentdir\$global:server\Launch-$global:server.ps1 -Force
         Add-Content -Path $global:currentdir\$global:server\Launch-$global:server.ps1 -Value "if(`$Null -eq (get-process `"$global:process`" -ea SilentlyContinue)){"
         Add-Content -Path $global:currentdir\$global:server\Launch-$global:server.ps1 -Value "Get-UpdateServer"
         Add-Content -Path $global:currentdir\$global:server\Launch-$global:server.ps1 -Value "Write-Host `"****   Server Starting  ****`" -ForegroundColor Magenta -BackgroundColor Black"
-        #Add-Content -Path $global:currentdir\$global:server\Launch-$global:server.ps1 -Value "Get-UpdateServer"
-        Add-Content -Path $global:currentdir\$global:server\Launch-$global:server.ps1 -Value "$global:currentdir\$global:server\RustDedicated.exe -batchmode +server.ip ${global:IP}  +server.port $global:PORT +server.tickrate $global:TICKRATE +server.hostname `"$global:HOSTNAME`" +server.maxplayers $global:MAXPLAYERS +server.worldsize $global:WORLDSIZE +server.saveinterval $global:SAVEINTERVAL +rcon.web $global:RCONWEB +rcon.ip ${global:IP} +rcon.port $global:RCONPORT +rcon.password `"$global:RCONPASSWORD`" "
+        Add-Content -Path $global:currentdir\$global:server\Launch-$global:server.ps1 -Value "Set-Location $global:currentdir\$global:server\ "
+        Add-Content -Path $global:currentdir\$global:server\Launch-$global:server.ps1 -Value ".\RustDedicated.exe -batchmode +server.ip ${global:IP}  +server.port $global:PORT +server.tickrate $global:TICKRATE +server.hostname `"$global:HOSTNAME`" +server.maxplayers $global:MAXPLAYERS +server.worldsize $global:WORLDSIZE +server.saveinterval $global:SAVEINTERVAL +rcon.web $global:RCONWEB +rcon.ip ${global:IP} +rcon.port $global:RCONPORT +rcon.password `"$global:RCONPASSWORD`" "
         Add-Content -Path $global:currentdir\$global:server\Launch-$global:server.ps1 -Value "}else{"
         Add-Content -Path $global:currentdir\$global:server\Launch-$global:server.ps1 -Value "Write-Host `"Server Running`""
         Add-Content -Path $global:currentdir\$global:server\Launch-$global:server.ps1 -Value "Get-Process `"$global:process`"}"
