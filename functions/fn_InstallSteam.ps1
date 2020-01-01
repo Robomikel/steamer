198b861af704721c834238b5cc0fc45dd14643cd
Robomikel <michaelawilliams28@outlook.com>
2019-12-31 19:29:14 -0700

    Added Message for Extraction

--- functions/fn_InstallSteam.ps1
+++ functions/fn_InstallSteam.ps1
@@ -14,9 +14,10 @@ Function Get-Steam
     {  
         #(New-Object Net.WebClient).DownloadFile("$global:steamurl", "steamcmd.zip")
         #####
-        Write-Host '*** Downloading and Extracting SteamCMD *****' -ForegroundColor Magenta -BackgroundColor Black  
+        Write-Host '*** Downloading SteamCMD *****' -ForegroundColor Cyan -BackgroundColor Black  
         Invoke-WebRequest -Uri $global:steamurl -OutFile $global:steamoutput
         Write-Output "Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)"
+        Write-Host '***  Extracting SteamCMD *****' -ForegroundColor Cyan -BackgroundColor Black 
         Expand-Archive "$global:currentdir\steamcmd.zip" "$global:currentdir\steamcmd\"
     }
 }
