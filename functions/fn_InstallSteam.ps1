e3b234f7a84884055c9523e64602c8f150fd3a9d
Robomikel <michaelawilliams28@outlook.com>
2020-01-01 12:39:01 -0700

    Updated Steam check

--- functions/fn_InstallSteam.ps1
+++ functions/fn_InstallSteam.ps1
@@ -5,8 +5,9 @@
 Function Get-Steam 
     {
         $start_time = Get-Date
-        $path = "$global:currentdir\steamcmd\" 
-    If(Test-Path $path) 
+        $path = "$global:currentdir\steamcmd\"
+        $patha = "$global:currentdir\steamcmd\steamcmd.exe" 
+    If((Test-Path $path) -and (Test-Path $patha)) 
     { 
         Write-Host 'steamCMD already downloaded!' -ForegroundColor Yellow -BackgroundColor Black
     } 
@@ -14,10 +15,10 @@ Function Get-Steam
     {  
         #(New-Object Net.WebClient).DownloadFile("$global:steamurl", "steamcmd.zip")
         #####
-        Write-Host '*** Downloading SteamCMD *****' -ForegroundColor Cyan -BackgroundColor Black  
+        Write-Host '*** Downloading SteamCMD *****' -ForegroundColor Magenta -BackgroundColor Black  
         Invoke-WebRequest -Uri $global:steamurl -OutFile $global:steamoutput
         Write-Output "Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)"
-        Write-Host '***  Extracting SteamCMD *****' -ForegroundColor Cyan -BackgroundColor Black 
+        Write-Host '***  Extracting SteamCMD *****' -ForegroundColor Magenta -BackgroundColor Black 
         Expand-Archive "$global:currentdir\steamcmd.zip" "$global:currentdir\steamcmd\"
     }
 }
