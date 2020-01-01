9b21e1c4dc28fc036f0914b52e13d9d7e65d10aa
Robomikel <michaelawilliams28@outlook.com>
2019-12-31 21:54:57 -0700

    Updated 7Zip exists test - removed copy

--- functions/fn_BackupServer.ps1
+++ functions/fn_BackupServer.ps1
@@ -1,43 +1,33 @@
 
 Function Get-ChecktaskDisable {
     Get-ScheduledTask -TaskName "$global:server monitor" >$null 2>&1
-if($?) {
+    if ($?) {
     #write-host "True, last operation succeeded"
     Write-Host '*** disabling scheduled task *****' -ForegroundColor Magenta -BackgroundColor Black
-    Disable-ScheduledTask -TaskName "$global:server monitor" >$null 2>&1
-}
+    Disable-ScheduledTask -TaskName "$global:server monitor" >$null 2>&1}
 
-if (!$?) {
+    if (!$?) {
     #write-host "Not True, last operation failed"
-    write-host "Scheduled Task does not exist" -ForegroundColor Yellow -BackgroundColor Black
-    
-}
-    
+    Write-Host "Scheduled Task does not exist" -ForegroundColor Yellow -BackgroundColor Black}
 }
 
 Function Get-ChecktaskEnable {
     Get-ScheduledTask -TaskName "$global:server monitor" >$null 2>&1
-if($?) {
+if ($?) {
     #write-host "True, last operation succeeded"
     Write-Host '*** Enabling scheduled task *****' -ForegroundColor Magenta -BackgroundColor Black
-    Enable-ScheduledTask -TaskName "$global:server monitor" >$null 2>&1
-    }
+    Enable-ScheduledTask -TaskName "$global:server monitor" >$null 2>&1}
 if (!$?) {
     #write-host "Not True, last operation failed"
-    write-host "Scheduled Task does not exist" -ForegroundColor Yellow -BackgroundColor Black
-    }
+    write-host "Scheduled Task does not exist" -ForegroundColor Yellow -BackgroundColor Black}
 }
 Function New-BackupFolder {
         $path = "$global:currentdir\backups" 
-    If(Test-Path $path) 
-    { 
-        Write-Host 'backup folder exists!' -ForegroundColor Yellow -BackgroundColor Black
-    } 
-    Else 
-    {  
+    If(Test-Path $path) { 
+        Write-Host '***  Backup folder exists! ***' -ForegroundColor Yellow -BackgroundColor Black} 
+    Else {  
         Write-Host '*** Creating backup folder *****' -ForegroundColor Magenta -BackgroundColor Black
-        New-Item -Path "$global:currentdir\" -Name "backups" -ItemType "directory"  
-    }
+        New-Item -Path "$global:currentdir\" -Name "backups" -ItemType "directory"}
 }
 
 Function New-BackupServer {
@@ -46,12 +36,12 @@ Function New-BackupServer {
     Get-StopServer
     Get-ChecktaskDisable 
     New-BackupFolder
-    Write-Host '*** Server Backup Started *****' -ForegroundColor Magenta -BackgroundColor Black
+    Write-Host '*** Server Backup Started! *****' -ForegroundColor Magenta -BackgroundColor Black
     Set-Location $global:currentdir\7za920\ 
     #./7za a $global:currentdir\backups\Backup_$global:server-$BackupDate.zip $global:currentdir\$global:server\* -an > backup.log
     ./7za a $global:currentdir\backups\Backup_$global:server-$BackupDate.zip $global:currentdir\$global:server\* > backup.log
     Get-ChecktaskEnable
-    Write-Host '*** Server Backup is Done *****' -ForegroundColor Yellow -BackgroundColor Black
+    Write-Host '*** Server Backup is Done! *****' -ForegroundColor Yellow -BackgroundColor Black
     .\backup.log
     Set-Location $global:currentdir
     #Set-Steamer
@@ -59,35 +49,46 @@ Function New-BackupServer {
 
 Function Get-SevenZip {
         $path = "$global:currentdir\7za920\"
-        $patha = "$global:currentdir\7za920-validate"
-        $path2 = "$global:currentdir\7za920-validate.zip"
+        $patha = "$global:currentdir\7za920\7za.exe"
         $pathb = "$global:currentdir\7za920.zip"
         Write-Host '*** Checking for 7ZIP *****' -ForegroundColor Yellow -BackgroundColor Black   
-    If((Test-Path $path) -and (Test-Path $path2) -and (Test-Path $patha) -and (Test-Path $pathb)) 
-    { 
-        search-sevenzip
-        if (!$?) {
-            write-host "7Zip files did not validate" -ForegroundColor Yellow -BackgroundColor Black
+    If((Test-Path $path) -and (Test-Path $patha) -and (Test-Path $pathb)) { 
+            Write-Host '*** 7Zip already downloaded! ****' -ForegroundColor Yellow -BackgroundColor Black}
+        else {
+            write-host "*** 7Zip not found!  ****" -ForegroundColor Yellow -BackgroundColor Black
             add-sevenzip
-            }
-        Write-Host '7Zip already downloaded!' -ForegroundColor Yellow -BackgroundColor Black
-    } 
-    Else 
-    {  
-        add-sevenzip
     }
+    
 }
-Function search-sevenzip {
-    Compare-Object $path $patha   >$null 2>&1
-    }
+
+
 Function add-sevenzip {
     Write-Host '*** Downloading 7ZIP *****' -ForegroundColor Magenta -BackgroundColor Black 
     (New-Object Net.WebClient).DownloadFile("$global:sevenzip", "$global:currentdir\7za920.zip")
-    Write-Host '***  Copying 7ZIP *****' -ForegroundColor Magenta -BackgroundColor Black 
-    Copy-Item $global:currentdir\7za920.zip $global:currentdir\7za920-validate.zip -Force
-    #Write-Host '*** Downloading and Extracting 7ZIP *****' -ForegroundColor Blue -BackgroundColor Black
-    Write-Host '***  Extracting 7ZIP *****' -ForegroundColor Magenta -BackgroundColor Black   
-    Expand-Archive "$global:currentdir\7za920-validate.zip" "$global:currentdir\7za920-validate" -Force
+    if (!$?) {write-host "*** 7Zip Download Failed ****" -ForegroundColor Yellow -BackgroundColor Black 
+    New-Tryagain}
+    if ($?) {write-host "*** 7Zip  Download succeeded ****" -ForegroundColor Yellow -BackgroundColor Black}
+    Write-Host '***  Extracting 7ZIP *****' -ForegroundColor Magenta -BackgroundColor Black 
     Expand-Archive "$global:currentdir\7za920.zip" "$global:currentdir\7za920\" -Force
-}    
-#################
+    if (!$?) {write-host "*** 7Zip files did not Failed  ****" -ForegroundColor Yellow -BackgroundColor Black
+    New-Tryagain}
+    if ($?) {write-host "*** 7Zip Extract succeeded ****" -ForegroundColor Yellow -BackgroundColor Black}
+}
+
+Function New-Tryagain {
+    $title    = 'Try again?'
+    $question = 'Download and Extract 7Zip?'
+
+    $choices = New-Object Collections.ObjectModel.Collection[Management.Automation.Host.ChoiceDescription]
+    $choices.Add((New-Object Management.Automation.Host.ChoiceDescription -ArgumentList '&Yes'))
+    $choices.Add((New-Object Management.Automation.Host.ChoiceDescription -ArgumentList '&No'))
+
+    $decision = $Host.UI.PromptForChoice($title, $question, $choices, 0)
+    if ($decision -eq 0) {
+        #Get-SourceMetaMod
+        Write-Host 'Entered Y'
+        add-sevenzip} 
+    else {
+        Write-Host 'Entered N'
+        exit}
+}
