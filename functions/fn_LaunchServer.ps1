0dd2779deb96cefd99c372c50ab743c8a23853ba
Robomikel <michaelawilliams28@outlook.com>
2020-01-02 00:14:47 -0700

    clean

--- functions/fn_LaunchServer.ps1
+++ functions/fn_LaunchServer.ps1
@@ -2,5 +2,4 @@ Function Select-launchServer {
     Write-Host '*** Starting Launch script *****' -ForegroundColor Yellow -BackgroundColor Black  
     & "$global:currentdir\$global:server\Launch-*.ps1"
     Set-Location $global:currentdir
-    #Select-Steamer
 }
