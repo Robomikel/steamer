5a833698618940c1455ca23e8cf3735223163085
Robomikel <michaelawilliams28@outlook.com>
2020-01-01 23:58:46 -0700

    Updated Monitor

--- functions/fn_CreateMonitor.ps1
+++ functions/fn_CreateMonitor.ps1
@@ -3,7 +3,6 @@
 Function Set-CreateMonitorScript{
 Write-Host '*** Creating Monitor Script *****' -ForegroundColor Magenta -BackgroundColor Black 
 New-Item $global:currentdir\$global:server\Monitor-$global:server.ps1 -Force
-#Add-Content -Path $global:currentdir\$global:server\Monitor-$global:server.ps1 -Value " "
 Add-Content -Path $global:currentdir\$global:server\Monitor-$global:server.ps1 -Value "`$mprocesss = Get-Process `"$global:process`""
 Add-Content -Path $global:currentdir\$global:server\Monitor-$global:server.ps1 -Value "If (!(`$mprocesss)) {"
 Add-Content -Path $global:currentdir\$global:server\Monitor-$global:server.ps1 -Value "& `"$global:currentdir\$global:server\Launch-*.ps1`""
