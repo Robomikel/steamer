bfa012f0b37fa7386a1b39d523aaec1441358c9f
Robomikel <michaelawilliams28@outlook.com>
2020-01-01 23:54:29 -0700

    Updated Message

--- functions/fn_CreateServerFolder.ps1
+++ functions/fn_CreateServerFolder.ps1
@@ -3,13 +3,13 @@ Function New-ServerFolder
     {   
         ##-- Create Folder for Server -- In current folder
         if((!$global:server) -or ($global:server -eq " ")){
-            Write-Host "You Entered a null or Empty" -ForegroundColor Red -BackgroundColor Black
+            Write-Host "*** You Entered a null or Empty ****" -ForegroundColor Red -BackgroundColor Black
             Select-Steamer
        }elseif (($global:AppID -eq "") -or ($global:AppID -eq " ")){
-            Write-Host "You Entered a space or Empty" -ForegroundColor Red -BackgroundColor Black
+            Write-Host "*** You Entered a space or Empty ****" -ForegroundColor Red -BackgroundColor Black
             Select-Steamer
         }elseif(Test-Path "$global:currentdir\$global:server\" ){
-            Write-Host 'Server Folder Already Created!' -ForegroundColor Yellow -BackgroundColor Black
+            Write-Host '*** Server Folder Already Created! ****' -ForegroundColor Yellow -BackgroundColor Black
         }else{
             Write-Host '*** Creating Server Folder *****' -ForegroundColor Magenta -BackgroundColor Black 
             New-Item -Path . -Name "$global:server" -ItemType "directory"
