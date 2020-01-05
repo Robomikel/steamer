48da26936fd1905c80e40931b32066d98627628f
Robomikel <michaelawilliams28@outlook.com>
2020-01-04 21:41:11 -0700

    Added Check ServerFolder Names Function

--- functions/fn_Actions.ps1
+++ functions/fn_Actions.ps1
@@ -102,3 +102,30 @@ function Get-TestString {
     exit
     }
 }
+
+Function Get-FolderNames {
+    if (Test-Path "$global:currentdir\$global:server\"){
+    }else{
+    New-ServerFolderq
+    }
+}
+Function New-ServerFolderq {
+    $title    = 'Server Folder Name does not exist!'
+    $question = 'Would you like to to create new Server Folder Name?'
+
+    $choices = New-Object Collections.ObjectModel.Collection[Management.Automation.Host.ChoiceDescription]
+    $choices.Add((New-Object Management.Automation.Host.ChoiceDescription -ArgumentList '&Yes'))
+    $choices.Add((New-Object Management.Automation.Host.ChoiceDescription -ArgumentList '&No'))
+
+    $decision = $Host.UI.PromptForChoice($title, $question, $choices, 1)
+    if ($decision -eq 0) {
+        Write-Host 'Entered Y'
+        $global:command = "install"
+        Select-Steamer $global:command $global:server
+        
+    } 
+    else {
+        Write-Host 'Entered N'
+        exit}
+}
+
