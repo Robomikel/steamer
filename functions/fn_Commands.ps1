97f7eeac4e7c78ed8be8886bc73e58a6570babc6
Robomikel <michaelawilliams28@outlook.com>
2020-01-04 21:41:03 -0700

    Added Check ServerFolder Names Function

--- functions/fn_Commands.ps1
+++ functions/fn_Commands.ps1
@@ -45,72 +45,86 @@ function Select-Steamer
             }elseif(($global:command -eq "update") -and ($null -eq $global:server)){
                 Get-logo
                 Write-Host 'Server FolderName for server updates: ' -ForegroundColor Cyan -NoNewline
-                $global:server = Read-host 
+                $global:server = Read-host
+                Get-FolderNames 
                 Get-createdvaribles
                 Get-UpdateServer
             }elseif($global:command -eq "update"){
                 Get-logo
+                Get-FolderNames
                 Get-createdvaribles
                 Get-UpdateServer
             }elseif(($global:command -eq "validate") -and ($null -eq $global:server)){
                 Get-logo
                 Write-Host 'Server FolderName for server validate: ' -ForegroundColor Cyan -NoNewline
-                $global:server = Read-host 
+                $global:server = Read-host
+                Get-FolderNames 
                 Get-createdvaribles
                 Get-ValidateServer
             }elseif($global:command -eq "validate"){
                 Get-logo
+                Get-FolderNames
                 Get-createdvaribles
                 Get-ValidateServer
             }elseif(($global:command -eq "start") -and ($null -eq $global:server)){
                 Get-logo
                 Write-Host 'Server FolderName for server launch, warning stops running process!: ' -ForegroundColor Cyan -NoNewline
-                $global:server = Read-host 
+                $global:server = Read-host
+                Get-FolderNames 
                 Select-launchServer
                 Get-ChecktaskEnable
             }elseif($global:command -eq "start"){
                 Get-logo
+                Get-FolderNames
                 Select-launchServer
                 Get-ChecktaskEnable
             }elseif(($global:command -eq "stop") -and ($null -eq $global:server)){
                 Get-logo
                 Write-Host 'Server FolderName for server stop, warning stops running process!: ' -ForegroundColor Cyan -NoNewline
-                $global:server = Read-host 
+                $global:server = Read-host
+                Get-FolderNames 
                 Get-createdvaribles
                 Get-StopServer
             }elseif($global:command -eq "stop"){
                 Get-logo
+                Get-FolderNames
                 Get-createdvaribles
                 Get-StopServer
             }elseif(($global:command -eq "restart") -and ($null -eq $global:server)){
                 Get-logo
                 Write-Host 'Server FolderName for server restart, warning stops running process!: ' -ForegroundColor Cyan -NoNewline
-                $global:server = Read-host 
+                $global:server = Read-host
+                Get-FolderNames 
                 Get-createdvaribles
                 Get-RestartsServer
             }elseif($global:command -eq "restart"){
                 Get-logo
+                Get-FolderNames
                 Get-createdvaribles
                 Get-RestartsServer   
             }elseif(($global:command -eq "check") -and ($null -eq $global:server)){
                 Get-logo
                 Write-Host 'Server FolderName for server check: ' -ForegroundColor Cyan -NoNewline
-                $global:server = Read-host 
+                $global:server = Read-host
+                Get-FolderNames 
                 Get-createdvaribles
                 Get-CheckServer
             }elseif($global:command -eq "check"){
                 Get-logo
+                Get-FolderNames
                 Get-createdvaribles
                 Get-CheckServer
             }elseif(($global:command -eq "backup") -and ($null -eq $global:server)){
                 Get-logo
                 Write-Host 'Server FolderName for server backup: ' -ForegroundColor Cyan -NoNewline
-                $global:server = Read-host 
+                $global:server = Read-host
+                Get-FolderNames 
                 Get-SevenZip
                 Get-createdvaribles
                 New-BackupServer
             }elseif($global:command -eq "backup"){
                 Get-logo
+                Get-FolderNames
                 Get-SevenZip
                 Get-createdvaribles
                 New-BackupServer  
@@ -118,16 +132,19 @@ function Select-Steamer
                 Get-logo
                 Write-Host 'Server FolderName for monitor: ' -ForegroundColor Cyan -NoNewline
                 $global:server = Read-host
+                Get-FolderNames
                 Get-ChecktaskUnreg 
                 #Set-MonitorJob
             }elseif($global:command -eq "monitor"){
                 Get-logo
+                Get-FolderNames
                 Get-ChecktaskUnreg
                 #Set-MonitorJob
             }elseif(($global:command -eq "gamedig") -and ($null -eq $global:server)){
                 Get-logo
                 Write-Host 'Server FolderName for gamedig: ' -ForegroundColor Cyan -NoNewline
-                $global:server = Read-host 
+                $global:server = Read-host
+                Get-FolderNames 
                 Get-NodeJS
                 Get-createdvaribles
                 if( $global:AppID -eq 581330) {  
@@ -138,6 +155,7 @@ function Select-Steamer
             }elseif($global:command -eq "gamedig"){
                 Get-logo
                 Get-NodeJS
+                Get-FolderNames
                 Get-createdvaribles
                 if( $global:AppID -eq 581330) {  
                     Get-GamedigServerQ
@@ -148,6 +166,7 @@ function Select-Steamer
                 Get-logo
                 Write-Host 'Server FolderName for gamedig: ' -ForegroundColor Cyan -NoNewline
                 $global:server = Read-host
+                Get-FolderNames
                 Get-NodeJS
                 Get-createdvaribles
                 if( $global:AppID -eq 581330) {  
@@ -158,6 +177,7 @@ function Select-Steamer
             }elseif($global:command -eq "gamedigPrivate"){
                 Get-logo
                 Get-NodeJS
+                Get-FolderNames
                 Get-createdvaribles
                 if( $global:AppID -eq 581330) {  
                     Get-GamedigServerQPrivate
