e74a18ec090000d4dd92ce9cb06b9b0ced8bcfc9
Robomikel <michaelawilliams28@outlook.com>
2020-01-10 23:15:30 -0700

    Updated Monitor

--- functions/fn_CreateMonitorJob.ps1
+++ functions/fn_CreateMonitorJob.ps1
@@ -1,6 +1,6 @@
 Function New-MontiorJob {
 Write-Host "Run Task only when user is logged on"
-$Action = New-ScheduledTaskAction -Execute 'powershell.exe' -Argument "$global:currentdir\$global:server\Monitor-$global:server.ps1" -WorkingDirectory "$global:currentdir\$global:server"
+$Action = New-ScheduledTaskAction -Execute 'powershell.exe' -Argument "-windowstyle hidden -file $global:currentdir\$global:server\Monitor-$global:server.ps1" -WorkingDirectory "$global:currentdir\$global:server"
 $Trigger = New-ScheduledTaskTrigger -Once -At (Get-Date).Date -RepetitionInterval (New-TimeSpan -Minutes 5) 
 $Settings = New-ScheduledTaskSettingsSet -ExecutionTimeLimit '00:00:00'
 $Task = New-ScheduledTask -Action $Action -Trigger $Trigger -Settings $Settings
