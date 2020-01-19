daac42ed512c3e20b03c735dc314cf8da487d649
Robomikel <michaelawilliams28@outlook.com>
2020-01-19 14:57:11 -0700

    Updated - final 1.0

--- functions/fn_Left4Dead2.ps1
+++ functions/fn_Left4Dead2.ps1
@@ -1,19 +1,17 @@
 Function New-LaunchScriptLFD2serverPS {
     #----------   left4dead2 Server CFG    -------------------
-    # Left 4 Dead 2 Server
     $global:MODDIR="left4dead2"
-    $global:GAME="left4dead2"
+    $global:EXEDIR=""
+    $global:GAME = "left4dead2"
     $global:PROCESS = "l4d2"
+    $global:SERVERCFGDIR = "left4dead2\cfg"
+    
     Get-StopServerInstall
+    $global:gamedirname="Left4Dead2"
+    $global:config1="server.cfg"
+    Get-Servercfg
+    # - - - - - - - - - - - - -
 
-    ${gamedirname}="Left4Dead2"
-    ${config1}="server.cfg"
-
-    Write-Host "***  Copying Default server.cfg  ***" -ForegroundColor Magenta -BackgroundColor Black
-    #(New-Object Net.WebClient).DownloadFile("$githuburl/${gamedirname}/${config1}", "$global:currentdir\$global:server\insurgency\cfg\server.cfg")
-    $lfdWebResponse=Invoke-WebRequest "$githuburl/${gamedirname}/${config1}"
-    New-Item $global:currentdir\$global:server\left4dead2\cfg\server.cfg -Force
-    Add-Content $global:currentdir\$global:server\left4dead2\cfg\server.cfg $lfdWebResponse
     Write-Host "***  Renaming srcds.exe to l4d2.exe to avoid conflict with local source Engine (srcds.exe) server  ***" -ForegroundColor Magenta -BackgroundColor Black
     Rename-Item -Path "$global:currentdir\$global:server\srcds.exe" -NewName "$global:currentdir\$global:server\l4d2.exe" >$null 2>&1
     Rename-Item -Path "$global:currentdir\$global:server\srcds_x64.exe" -NewName "$global:currentdir\$global:server\l4d2_x64.exe" >$null 2>&1
@@ -33,13 +31,13 @@ Function New-LaunchScriptLFD2serverPS {
     #if(($global:workshop = Read-Host -Prompt (Write-Host "Input 1 to enable workshop, Press enter to accept default value [0]: "-ForegroundColor Cyan -NoNewline)) -eq ''){$global:workshop="0"}else{$global:workshop}
     #if(($global:sv_pure = Read-Host -Prompt (Write-Host "Input addtional launch params ie. +sv_pure 0, Press enter to accept default value []: "-ForegroundColor Cyan -NoNewline)) -eq ''){}else{$global:sv_pure}
     Write-Host "***  Editing Default server.cfg  ***" -ForegroundColor Magenta -BackgroundColor Black
-    ((Get-Content -path $global:currentdir\$global:server\left4dead2\cfg\server.cfg -Raw) -replace "\bSERVERNAME\b","$global:HOSTNAME") | Set-Content -Path $global:currentdir\$global:server\left4dead2\cfg\server.cfg
-    ((Get-Content -path $global:currentdir\$global:server\left4dead2\cfg\server.cfg -Raw) -replace "\bADMINPASSWORD\b","$global:RCONPASSWORD") | Set-Content -Path $global:currentdir\$global:server\left4dead2\cfg\server.cfg
+    ((Get-Content -path $global:currentdir\$global:server\$global:SERVERCFGDIR\server.cfg -Raw) -replace "\bSERVERNAME\b","$global:HOSTNAME") | Set-Content -Path $global:currentdir\$global:server\$global:SERVERCFGDIR\server.cfg
+    ((Get-Content -path $global:currentdir\$global:server\$global:SERVERCFGDIR\server.cfg -Raw) -replace "\bADMINPASSWORD\b","$global:RCONPASSWORD") | Set-Content -Path $global:currentdir\$global:server\$global:SERVERCFGDIR\server.cfg
     Write-Host "***  Creating Launch script ***" -ForegroundColor Magenta -BackgroundColor Black
     New-Item $global:currentdir\$global:server\Launch-$global:server.ps1 -Force
     Add-Content -Path $global:currentdir\$global:server\Launch-$global:server.ps1 -Value "Write-Host `"****   Server Starting  ****`" -ForegroundColor Magenta -BackgroundColor Black"
     Add-Content -Path $global:currentdir\$global:server\Launch-$global:server.ps1 -Value "Set-Location $global:currentdir\$global:server\"
-    Add-Content -Path $global:currentdir\$global:server\Launch-$global:server.ps1 -Value "$global:currentdir\$global:server\l4d2.exe -console -game left4dead2 -strictportbind -ip `${global:IP} +port `$global:PORT +clientport `$global:CLIENTPORT +hostip ${global:EXTIP} +maxplayers `$global:MAXPLAYERS +map '`$global:MAP' -condebug "# +sv_workshop_enabled $global:workshop $global:sv_pure
+    Add-Content -Path $global:currentdir\$global:server\Launch-$global:server.ps1 -Value "$global:currentdir\$global:server\l4d2.exe -console -game left4dead2 -strictportbind -ip `${global:IP} +port `$global:PORT +clientport `$global:CLIENTPORT +hostip ${global:EXTIP} +maxplayers `$global:MAXPLAYERS +map `"`$global:MAP`" -condebug "# +sv_workshop_enabled $global:workshop $global:sv_pure
     #A:\L4D2\srcds.exe -console -game left4dead2 -ip 10.0.0.2 +port 27020 +hostip YOURDEDIIP +maxplayers 8 +exec server.cfg +map c2m1_highway
     # -game left4dead2 -strictportbind -ip ${ip} -port ${port} +clientport ${clientport} +map ${defaultmap} +servercfgfile ${servercfg} -maxplayers ${maxplayers}
     Get-SourceMetMod
