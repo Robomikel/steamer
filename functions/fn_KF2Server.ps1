7bc62b1a35bcf5cccab59e493898f4754a40fc5a
Robomikel <michaelawilliams28@outlook.com>
2020-01-19 01:38:51 -0700

    updated killing floor 2

--- functions/fn_KF2Server.ps1
+++ functions/fn_KF2Server.ps1
@@ -1,42 +1,32 @@
 Function New-LaunchScriptKF2serverPS {
     # Killing Floor 2 Server
+    # - - - - - - - - - - - -
     $global:EXEDIR="Binaries\Win64"
     $global:GAME = "killingfloor2"
     $global:PROCESS = "KFserver"
+    $global:SERVERCFGDIR = "\KFGame\Config"
+    $global:EXIDIR=""
+    #$global:SERVERCFG="server.cfg"
+    
     Get-StopServerInstall
     
-    ${gamedirname}="KillingFloor2"
-    ${config1}="KFWeb.ini"
-    ${config2}="LinuxServer-KFEngine.ini"
-    ${config3}="LinuxServer-KFGame.ini"
-    ${config4}="LinuxServer-KFInput.ini"
-    ${config5}="LinuxServer-KFSystemSettings.ini"
-    Write-Host "***  Copying Default KFWeb.ini ***" -ForegroundColor Magenta -BackgroundColor Black
-    $kf2WebResponse=Invoke-WebRequest "$githuburl/${gamedirname}/${config1}"
-    New-Item $global:currentdir\$global:server\KFGame\Config\KFWeb.ini -Force
-    Add-Content $global:currentdir\$global:server\KFGame\Config\KFWeb.ini $kf2WebResponse
-    Write-Host "***  Copying Default PCServer-KFEngine.ini ***" -ForegroundColor Magenta -BackgroundColor Black
-    $kf2WebResponse=Invoke-WebRequest "$githuburl/${gamedirname}/${config2}"
-    New-Item $global:currentdir\$global:server\KFGame\Config\PCServer-KFEngine.ini -Force
-    Add-Content $global:currentdir\$global:server\KFGame\Config\PCServer-KFEngine.ini $kf2WebResponse
-    Write-Host "***  Copying Default PCServer-KFGame.ini ***" -ForegroundColor Magenta -BackgroundColor Black
-    $kf2WebResponse=Invoke-WebRequest "$githuburl/${gamedirname}/${config3}"
-    New-Item $global:currentdir\$global:server\KFGame\Config\PCServer-KFGame.ini -Force
-    Add-Content $global:currentdir\$global:server\KFGame\Config\PCServer-KFGame.ini $kf2WebResponse
-    Write-Host "***  Copying Default PCServer-KFInput.ini ***" -ForegroundColor Magenta -BackgroundColor Black
-    $kf2WebResponse=Invoke-WebRequest "$githuburl/${gamedirname}/${config4}"
-    New-Item $global:currentdir\$global:server\KFGame\Config\PCServer-KFInput.ini -Force
-    Add-Content $global:currentdir\$global:server\KFGame\Config\PCServer-KFInput.ini $kf2WebResponse
-    Write-Host "***  Copying Default PCServer-KFSystemSettings.ini  ***" -ForegroundColor Magenta -BackgroundColor Black
-    $kf2WebResponse=Invoke-WebRequest "$githuburl/${gamedirname}/${config5}"
-    New-Item $global:currentdir\$global:server\KFGame\Config\PCServer-KFSystemSettings.ini -Force
-    Add-Content $global:currentdir\$global:server\KFGame\Config\PCServer-KFSystemSettings.ini $kf2WebResponse
-    
+    $global:gamedirname="KillingFloor2"
+    $global:config1="KFWeb.ini"
+    $global:config2="LinuxServer-KFEngine.ini"
+    $global:config3="LinuxServer-KFGame.ini"
+    $global:config4="LinuxServer-KFInput.ini"
+    $global:config5="LinuxServer-KFSystemSettings.ini"
+    Write-Host "***  Copying Default Config ***" -ForegroundColor Magenta -BackgroundColor Black
+    Remove-item $global:currentdir\$global:server\$global:SERVERCFGDIR\PCServer-*.ini -Force  >$null 2>&1
+    Get-Servercfg
+    Set-Location $global:currentdir\$global:server\$global:SERVERCFGDIR
+    Get-ChildItem -Filter "LinuxServer-*.ini" -Recurse | Rename-Item -NewName {$_.name -replace 'LinuxServer','PCServer'} -Force
+    # - - - - - - - - - - - - -
     Write-Host '*** Configure Instance *****' -ForegroundColor Yellow -BackgroundColor Black
     #Write-Host "Input Server local IP: " -ForegroundColor Cyan -NoNewline
     #${global:IP} = Read-Host
     Write-Host "Changing the Port will change the query Port. N+? if not sure keep default" -ForegroundColor Yellow
-    if(($global:PORT = Read-Host -Prompt (Write-Host "Input Server Port,Press enter to accept default value [7787]: "-ForegroundColor Cyan -NoNewline)) -eq ''){$global:PORT="7787"}else{$global:PORT}
+    if(($global:PORT = Read-Host -Prompt (Write-Host "Input Server Port,Press enter to accept default value [7777]: "-ForegroundColor Cyan -NoNewline)) -eq ''){$global:PORT="7777"}else{$global:PORT}
     if(($global:QUERYPORT = Read-Host -Prompt  (Write-Host "Input Server Query Port, Press enter to accept default value [27015]: " -ForegroundColor Cyan -NoNewline)) -eq ''){$global:QUERYPORT="27015"}else{$global:QUERYPORT}
     if(($global:MAP = Read-Host -Prompt (Write-Host "Input Server Map, Press enter to accept default value [KF-BioticsLab]: " -ForegroundColor Cyan -NoNewline)) -eq ''){$global:MAP="KF-BioticsLab"}else{$global:MAP}
     if(($global:GAMEMODE = Read-Host -Prompt (Write-Host "Input gamemode, Press enter to accept default value [KFGameContent.KFGameInfo_Endless]: "-ForegroundColor Cyan -NoNewline)) -eq ''){$global:GAMEMODE="KFGameContent.KFGameInfo_Endless"}else{$global:GAMEMODE}
@@ -48,7 +38,7 @@ Function New-LaunchScriptKF2serverPS {
     New-Item $global:currentdir\$global:server\Launch-$global:server.ps1 -Force
     Add-Content -Path $global:currentdir\$global:server\Launch-$global:server.ps1 -Value "Write-Host `"****   Server Starting  ****`" -ForegroundColor Magenta -BackgroundColor Black"
     Add-Content -Path $global:currentdir\$global:server\Launch-$global:server.ps1 -Value "Set-Location $global:currentdir\$global:server\Binaries\Win64"
-    Add-Content -Path $global:currentdir\$global:server\Launch-$global:server.ps1 -Value "./KFServer.exe `$global:MAP`?Game=`$global:GAMEMODE`?Difficulty=`$global:DIFF`? -Port=`$global:PORT -QueryPort=`$global:QUERYPORT"
+    Add-Content -Path $global:currentdir\$global:server\Launch-$global:server.ps1 -Value "./KFServer.exe `"`$global:MAP`"`?Game=`"`$global:GAMEMODE`"`?Difficulty=`"`$global:DIFF`"`? -Port=`"`$global:PORT`" -QueryPort=`"`$global:QUERYPORT`""
     Write-Host "***  starting Server before Setting PCServer-KFGame.ini Please Wait ***" -ForegroundColor Magenta -BackgroundColor Black
     Select-launchServer
     timeout 5
