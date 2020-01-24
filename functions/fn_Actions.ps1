b563d6b6e69b98a7926f2e4158306c1c58d8a220
Robomikel <michaelawilliams28@outlook.com>
2020-01-24 16:44:32 -0700

    fixed mcrcon message

--- functions/fn_Actions.ps1
+++ functions/fn_Actions.ps1
@@ -584,7 +584,7 @@ Function Get-MCRcon {
     $path = "$global:currentdir\mcrcon\"
     $patha = "$global:currentdir\mcrcon\mcrcon-0.7.1-windows-x86-32\mcrcon.exe" 
     If((Test-Path $path) -and (Test-Path $patha)){ 
-    Write-Host 'mcrcon already downloaded!' -ForegroundColor Yellow -BackgroundColor Black} 
+    Write-Host '****   mcrcon already downloaded!   ****' -ForegroundColor Yellow -BackgroundColor Black} 
     Else{  
     $start_time = Get-Date
     Write-Host '****   Downloading MCRCon from github   ****' -ForegroundColor Magenta -BackgroundColor Black 
