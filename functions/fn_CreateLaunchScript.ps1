11baa55e3ffd25f61cce24217dd9e54b696c7345
Robomikel <michaelawilliams28@outlook.com>
2020-01-31 15:54:51 -0700

    added day of defeat source

--- functions/fn_CreateLaunchScript.ps1
+++ functions/fn_CreateLaunchScript.ps1
@@ -656,12 +656,12 @@ Function New-LaunchScriptBlackMesaserverPS {
         Get-Servercfg
         #--->Default Vars
         $global:RCONPORT = "${global:PORT}"
-        $global:defaultip="${global:ip}"
-        $global:defaultport="27015"
-        $global:defaultclientport="27005"
-        $global:defaultsourcetvport="27020"
-        $global:defaultdefaultmap="dm_bounce"
-        $global:defaultmaxplayers="16"
+        $global:defaultip = "${global:ip}"
+        $global:defaultport = "27015"
+        $global:defaultclientport = "27005"
+        $global:defaultsourcetvport = "27020"
+        $global:defaultdefaultmap = "dm_bounce"
+        $global:defaultmaxplayers = "16"
 
         #--->input questions 
         Get-UserInput 1 1 0 0 1 1 0 1 1 1 1 1 0 0 0 0
@@ -678,12 +678,59 @@ Function New-LaunchScriptBlackMesaserverPS {
         #$global:launchParams = '@("$global:EXEDIR\$global:EXE -< LAUNCH PARAMS HERE >-")'
 }
 
+Function New-LaunchScriptTEMPLATEserverPS {
+        #* * Add to Read-AppID in fn_Actions.ps1 * *
+        # Day of Defeat Dedicated Server
+        # APP ID # 232290  
+        # https://kb.firedaemon.com/support/solutions/articles/4000086944-day-of-defeat-source
+        # Requiered Dont change 
+        # # Version 2.0
+        #--->Requieres \/ \/ Get-SourceMetMod
+        $global:MODDIR = ""
+        #--->Exe NOT in root server folder \/\/
+        $global:EXEDIR = ""
+        #--->rename srcds to this name \/\/
+        $global:EXE = ""
+        #--->Requieres \/ \/ game dig 
+        $global:GAME = "dods"
+        #--->Requieres \/ \/ AppData Roaming save
+        $global:SAVES = ""
+        #--->Requieres \/ \/ maybe same as game exe?
+        $global:PROCESS = "dods"
+        #--->game config folder
+        $global:SERVERCFGDIR = ""
+        #--->Stop existing process if running        
+        Get-StopServerInstall
+        #--->Game-server-manger folder \/
+        $global:gamedirname = "DayOfDefeat"
+        #--->Game-server-manger config name \/
+        $global:config1 = "server.cfg"
+        #--->Get game-server-config \/\/
+        Get-Servercfg
+        #--->Default Vars
+        $global:defaultip = "0.0.0.0"
+        $global:defaultport = "27015"
+        #$global:defaultclientport="27005"
+        $global:defaultmap = "dod_Anzio"
+        $global:defaultmaxplayers = "16"
+        #--->input questions 1 1 0 0 0 0 0 1 0 1 1 0 0
+        Get-UserInput 1 1 0 0 0 0 0 1 0 1 0 0 0 
+        #--->rename srcds.exe \/\/
+        Select-RenameSource
+        #--->Edit game config \/ SERVERNAME ADMINPASSWORD
+        Select-EditSourceCFG
+        # --->Launch 
+        $global:launchParams = '@("$global:EXE -console -game `"dods`" -secure +map ${defaultmap} -autoupdate +log on +maxplayers ${maxplayers} -port ${port}  +ip ${ip} +exec server.cfg")'
+        # $global:launchParams = '@("$global:EXE -console -game dod -strictportbind +ip ${ip} -port ${port} +clientport ${clientport} +map ${defaultmap} +servercfgfile server.cfg -maxplayers ${maxplayers}")'
+        # OR    EXE NOT In server folder ROOT add EXEDIR \/ \/
+        #$global:launchParams = '@("$global:EXEDIR\$global:EXE -< LAUNCH PARAMS HERE >-")'
+}
 
 
 
 #Function New-LaunchScriptTEMPLATEserverPS {
 #        * * Add to Read-AppID in fn_Actions.ps1 * *
-# TEMPLATE Server
+# TEMPLATE Dedicated Server
 # APP ID #
 # WIKI
 # Requiered Dont change 
