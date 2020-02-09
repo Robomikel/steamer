007e0c4d21076abe94c434965b0b77b7179672ed
Robomikel <michaelawilliams28@outlook.com>
2020-02-09 00:27:34 -0700

    updated 7days to die launch

--- functions/fn_CreateLaunchScript.ps1
+++ functions/fn_CreateLaunchScript.ps1
@@ -169,7 +169,8 @@ Function New-LaunchScriptSdtdserverPS {
         #----------   7Days2Die Ask for input for server cfg    -------------------
         # APP ID # 294420
         $global:MODDIR = ""
-        $global:EXE = "startdedicated.bat"
+        #$global:EXE = "startdedicated.bat"
+        $global:EXE = "7DaysToDieServer"
         $global:EXEDIR = ""
         $global:GAME = "7d2d"
         $global:SAVES = "7DaysToDie"
@@ -179,7 +180,10 @@ Function New-LaunchScriptSdtdserverPS {
         $global:gamedirname = ""
         $global:config1 = "serverconfig.xml"
         # Get-Servercfg
-        # - - - - - - - - - - - - -
+        IF (Test-Path $global:currentdir\$global:server\7DaysToDieServer.exe){
+        }Else{
+            $global:EXE = "7DaysToDie"
+        }
         Write-Host '*** Configure Instance *****' -ForegroundColor Yellow -BackgroundColor Black
         if (($global:PORT = Read-Host -Prompt (Write-Host "Input Server Port,Press enter to accept default value [26900]: " -ForegroundColor Cyan -NoNewline)) -eq '') { $global:PORT = "26900" }else { $global:PORT }
         Write-Host 'Input Server name: ' -ForegroundColor Cyan -NoNewline
@@ -187,7 +191,8 @@ Function New-LaunchScriptSdtdserverPS {
         ((Get-Content -path $global:currentdir\$global:server\$global:config1 -Raw) -replace "My Game Host", "$global:HOSTNAME") | Set-Content -Path $global:currentdir\$global:server\$global:config1 
         ((Get-Content -path $global:currentdir\$global:server\$global:config1 -Raw) -replace '26900', "$global:PORT") | Set-Content -Path $global:currentdir\$global:server\$global:config1 
         ((Get-Content -path $global:currentdir\$global:server\startdedicated.bat -Raw) -replace 'pause', 'exit') | Set-Content -Path $global:currentdir\$global:server\startdedicated.bat        
-        $global:launchParams = '@("$global:EXE")'
+        $global:launchParams = '@("$global:EXE -logfile $global:currentdir\$global:server\server.log -quit -batchmode -nographics -configfile=serverconfig.xml -dedicated")'
+        #$global:launchParams = '@("$global:EXE")'
 
 }
 
