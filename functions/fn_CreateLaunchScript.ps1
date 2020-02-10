c09fc3eea90b9ecf68d04cfb107c4a7e43b71698
Robomikel <michaelawilliams28@outlook.com>
2020-02-10 01:27:47 -0700

    added dont starve together

--- functions/fn_CreateLaunchScript.ps1
+++ functions/fn_CreateLaunchScript.ps1
@@ -690,8 +690,24 @@ Function New-LaunchScriptDSTserverPS {
         # Don't Starve Together Dedicated Server
         # APP ID # 343050
         # https://steamcommunity.com/sharedfiles/filedetails/?id=590681995
-        # Requiered Dont change
-        # # Version 2.0
+        ################## Change Default Variables #################
+        #                       Server IP 
+        ${global:IP}            = "${global:IP}"
+        #                       Server Port
+        $global:port            = "10999"
+        #                       RCON Port
+        $global:RCONPORT        = "${global:PORT}"
+        #                       Maxplayers
+        $global:maxplayers      = "32"
+        #                       Server Name
+        $global:HOSTNAME        = "$env:USERNAME"
+    
+        ##############################/\##############################
+        
+        
+        
+        
+        ###################### Do not change below #####################
         #--->Exe NOT in root server folder \/\/
         $global:EXEDIR = "bin"
         #--->rename srcds to this name \/\/
@@ -700,50 +716,51 @@ Function New-LaunchScriptDSTserverPS {
         $global:GAME = "protocol-valve"
         #--->Requieres \/ \/ maybe same as game exe?
         $global:PROCESS = "dontstarve_dedicated_server_nullrenderer"
-        #--->game config folder
-        #$global:SERVERCFGDIR = ""
+    
         #--->Stop existing process if running
         Get-StopServerInstall
         #--->Game-server-manger folder \/
         $global:gamedirname = "DontStarveTogether"
-
-        $shard = "Master"
-        $shard2 = "Cave"
-        $cluster = "Cluster_1"
-        ${persistentstorageroot} = "C:\users\$env:USERNAME\Klei\$global:gamedirname "
+    
+        $global:shard = "Master"
+        $global:cluster = "Cluster_1"
+        # moved to server folder root
+        $global:persistentstorageroot = "Klei"
         
         #--->Game-server-manger config name \/
-        $global:SERVERCFGDIR = "$persistentstorageroot\$cluster\$shard\"
+        $global:SERVERCFGDIR = "$global:persistentstorageroot\$global:gamedirname\$global:cluster\$global:shard\"
         $global:config1 = "server.ini"
         #--->Get game-server-config \/\/
         Get-Servercfg
         
         #--->Game-server-manger config name \/
-        $global:SERVERCFGDIR = "$persistentstorageroot\$cluster"
-        $global:config2 = "cluster.ini"
+        $global:SERVERCFGDIR = "$global:persistentstorageroot\$global:gamedirname\$global:cluster"
+        $global:config1 = "cluster.ini"
         #--->Get game-server-config \/\/
         Get-Servercfg
-             
+                     
         #--->Default Vars
         $global:RCONPORT = "${global:PORT}"
         $global:defaultip = "${global:IP}"
         $global:defaultport = "10999"
         $global:defaultmaxplayers = "32"
-        $global:defaultHOSTNAME = "PS Steamer"
+        $global:defaultHOSTNAME = "$env:username"
         $global:defaultRCONPASSWORD = "$global:RANDOMPASSWORD"
         #--->input questions 1 1 0 0 0 0 0 1 0 1 1 0 0
         Get-UserInput 1 1 0 0 0 1 1 0 0 0 0 0
 
         #--->Edit game config \/ SERVERNAME ADMINPASSWORD
         Select-EditSourceCFG
-
         # BOTH CAVES AND MASTER
-        # $global:launchParams = '@("$global:EXEDIR\$global:EXE -console -cluster ${cluster} -shard ${shard} -backup_logs ;; $global:EXEDIR\$global:EXE -console -cluster ${cluster} -shard ${shard2} -backup_logs")'
+        #$global:launchParams = '@("$global:EXE -console -bind_ip ${global:ip} -port ${global:PORT} -players ${global:maxplayers} -persistent_storage_root $global:currentdir\$global:server\${global:persistentstorageroot} -conf_dir ${global:gamedirname} -cluster ${global:cluster} -shard ${global:shard} -backup_logs ; ; $global:EXE -console -persistent_storage_root $global:currentdir\$global:server\${global:persistentstorageroot} -conf_dir ${global:gamedirname} -cluster ${global:cluster} -shard cave -backup_logs")'
+        
         # Master
-        $global:launchParams = '@("$global:EXEDIR\$global:EXE -console -bind_ip ${global:ip} -port ${global:PORT} -players ${global:maxplayers} -persistent_storage_root ${persistentstorageroot} -conf_dir ${global:gamedirname} -cluster ${cluster} -shard ${shard} -backup_logs -condebug")'
+        $global:launchParams = '@("$global:EXE -console -bind_ip ${global:ip} -port ${global:PORT} -players ${global:maxplayers} -persistent_storage_root $global:currentdir\$global:server\${global:persistentstorageroot} -conf_dir ${global:gamedirname} -cluster ${global:cluster} -shard ${global:shard} -backup_logs")'
+        
         # Caves
-        #$global:launchParams = '@("$global:EXEDIR\$global:EXE -console -bind_ip ${global:ip} -port ${global:PORT} -players ${global:maxplayers} -persistent_storage_root ${persistentstorageroot} -conf_dir ${global:gamedirname} -cluster ${cluster} -shard ${shard} -backup_logs")'
-}
+        #$global:launchParams = '@("$global:EXE -console -bind_ip ${global:ip} -port ${global:PORT} -players ${global:maxplayers} -persistent_storage_root $global:currentdir\$global:server\${global:persistentstorageroot} -conf_dir ${global:gamedirname} -cluster ${global:cluster} -shard ${global:cave} -backup_logs")'
+    }
+
 Function New-LaunchScriptGMODserverPS {
         #        * * Add to Read-AppID in fn_Actions.ps1 * *
         # Garry's Mod Dedicated Server
